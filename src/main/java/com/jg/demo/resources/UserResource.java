38a73b3d40eeb53e1d06a2bf966e0b281e8b288c
Juliana Guonik <juliana_guonik@hotmail.com>
2022-10-18 10:14:36 -0300

    User entity and resource

---
+++ src/main/java/com/jg/demo/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.jg.demo.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jg.demo.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Mary", "mary@mail.com", "123456");
+		return ResponseEntity.ok().body(u);
+		
+		
+	}
+
+}
