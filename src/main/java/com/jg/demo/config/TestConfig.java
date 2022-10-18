c97400257c0ca8ccccf2c1c7d67203935356abcc
Juliana Guonik <juliana_guonik@hotmail.com>
2022-10-18 14:11:44 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/jg/demo/config/TestConfig.java
@@ -0,0 +1,30 @@
+package com.jg.demo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.jg.demo.entities.User;
+import com.jg.demo.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		User u1 = new User(null, "Maria Brown", "mariab@mail.com", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "123456");
+		
+		userRepository.saveAll(Arrays.asList(u1, u2));
+	}
+	
+	
+
+}
---
+++ src/main/java/com/jg/demo/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.jg.demo.repositories;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jg.demo.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	
+
+}
