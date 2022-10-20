219965f7952df3a68f9274764e32d46b3aa96831
Juliana Guonik <juliana_guonik@hotmail.com>
2022-10-20 10:00:29 -0300

    Exception handling - update

--- src/main/java/com/jg/demo/services/UserService.java
+++ src/main/java/com/jg/demo/services/UserService.java
@@ -3,6 +3,8 @@ package com.jg.demo.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
-		return repository.save(entity);
+		return repository.save(entity); 
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 		
 	}
 
